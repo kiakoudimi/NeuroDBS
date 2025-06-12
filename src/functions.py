78345b0c9e4b632431228114bb7484e9491259d0
kiakoud <kiakoud@cbs.mpg.de>
2025-06-12 14:13:35 +0200

    updated

--- src/functions.py
+++ src/functions.py
@@ -83,19 +83,7 @@ def get_data(root, mask_img, ids=None):
 
 
 def plot_auc_heatmap(file_path, model_list):
-    """
-    Generate a square-like heatmap plot of AUC scores for each model-feature map pair.
-
-    Args:
-        file_path (str or Path): Path to the AUC results. Each sheet should
-                                 represent one dataset and contain model names in the first column
-                                 and corresponding AUC scores in the second column
-        model_list (list of str): List of model names
-
-    Returns:
-        None: display and save the plot.
-
-    """
+    
     xls = pd.ExcelFile(file_path)
 
     dataset_list = []
@@ -103,26 +91,30 @@ def plot_auc_heatmap(file_path, model_list):
 
     # Get perfrormences per feature map
     for sheet_name in xls.sheet_names:
-        df_results = pd.read_excel(xls, sheet_name=sheet_name)
+        df = pd.read_excel(xls, sheet_name=sheet_name)
 
-        # Get model names
-        models = df_results.iloc[:, 0]
+        #Get model names
+        models = df.iloc[:, 0]
 
-        # Get AUC scores
-        mean_auc = df_results.iloc[:, 1]
+        #Get AUC scores
+        mean_auc = df.iloc[:, 1]
 
         # Add data to the lists
-        dataset_list.extend([sheet_name] * len(models))
+        dataset_list.extend([sheet_name] * len(models))  
         auc_list.extend(mean_auc)
 
     # Results export
-    auc_long = pd.DataFrame({"Model": model_list, "Dataset": dataset_list, "AUC": auc_list})
+    auc_long = pd.DataFrame({
+        'Model': model_list,
+        'Dataset': dataset_list,
+        'AUC': auc_list
+    })
 
     # Remove underscores from names
-    auc_long["Dataset"] = auc_long["Dataset"].str.replace("_", " ", regex=False)
+    auc_long['Dataset'] = auc_long['Dataset'].str.replace('_', ' ', regex=False)
 
-    # Plot figure heatmap
-    plt.figure(figsize=(13, 8))
+    #Plot figure heatmap
+    fig =plt.figure(figsize=(13, 8))
     ax = sns.scatterplot(
         data=auc_long,
         x="Dataset",
@@ -132,32 +124,31 @@ def plot_auc_heatmap(file_path, model_list):
         palette="RdYlBu",
         marker="s",
         sizes=(1000, 2400),
-        legend=False,
+        legend=False  
     )
 
     # Add text annotations
     for i in range(len(auc_long)):
         ax.text(
-            x=auc_long["Dataset"][i],
-            y=auc_long["Model"][i],
-            s=f"{auc_long['AUC'][i]:.2f}",
-            color="black",
-            ha="center",
-            va="center",
-            fontsize=13,
+        x=auc_long["Dataset"][i],
+        y=auc_long["Model"][i],
+        s=f"{auc_long['AUC'][i]:.2f}",
+        color="black",
+        ha="center", va="center",
+        fontsize=13
         )
 
-    norm = plt.Normalize(vmin=auc_long["AUC"].min(), vmax=auc_long["AUC"].max())
+    norm = plt.Normalize(vmin=auc_long['AUC'].min(), vmax=auc_long['AUC'].max())
     sm = plt.cm.ScalarMappable(cmap="RdYlBu", norm=norm)
     sm.set_array([])
 
     # Add the colorbar with min/max ticks
     cbar = plt.colorbar(sm, ax=ax)
-    cbar.set_label("AUC Score", fontsize=15)
+    cbar.set_label('AUC Score', fontsize=15)
 
-    # Round the min and max AUC values
-    min_auc_rounded = round(auc_long["AUC"].min(), 2)
-    max_auc_rounded = round(auc_long["AUC"].max(), 2)
+    # Round the min and max AUC values 
+    min_auc_rounded = round(auc_long['AUC'].min(), 2)
+    max_auc_rounded = round(auc_long['AUC'].max(), 2)
 
     # Set ticks to show the rounded min and max values
     cbar.set_ticks([min_auc_rounded, max_auc_rounded])
@@ -166,25 +157,12 @@ def plot_auc_heatmap(file_path, model_list):
     plt.xticks(fontsize=15, rotation=45)
     plt.yticks(fontsize=15)
 
-    plt.tight_layout()
-    plt.savefig("heatmap.svg")
-    plt.show()
-
+    plt.tight_layout()  
+    plt.close(fig)
+    return fig
 
 def plot_metrics(file_path, metrics, model_names):
-    """
-    Plot metrics across feature maps and models.
 
-    Args:
-        file_path (str or Path): Path to results (.xlsx). Each sheet should contain metric values
-                                 for each model, with model names in the first column.
-        metrics (list of str): List of metric names to plot (e.g., ["AUC", "F1"]).
-        model_names (list of str): Ordered list of model names to display on the x-axis.
-
-    Returns:
-        None: display and save the plot.
-
-    """
     # Read data
     xls = pd.ExcelFile(file_path)
     datasets = xls.sheet_names
@@ -193,38 +171,37 @@ def plot_metrics(file_path, metrics, model_names):
 
     # Get data
     for dataset in datasets:
-        df_map = xls.parse(dataset)
-        df_map.set_index(df_map.columns[0])
-        df_map.index = model_names
+        df = xls.parse(dataset)
+        df.set_index(df.columns[0], inplace=True)
+        df.index = model_names
 
         # Extract values for each metric
         for metric in metrics:
-            if metric in df_map.columns:
-                data_for_plot[metric][dataset] = df_map[metric]
+            if metric in df.columns:
+                data_for_plot[metric][dataset] = df[metric]
 
     # Create subplots for each metric
-    _, axes = plt.subplots(len(metrics), 1, figsize=(7, 5 * len(metrics)))
+    fig, axes = plt.subplots(len(metrics), 1, figsize=(7, 5.3 * len(metrics)))
 
     # Loop through each metric and plot it
     for idx, metric in enumerate(metrics):
         ax = axes[idx]
-        for dataset in datasets:
+        for i, dataset in enumerate(datasets):
             metric_values = data_for_plot[metric].get(dataset)
             if metric_values is not None:
                 x_pos = range(len(model_names))
-                ax.scatter(
-                    x_pos, metric_values, label=dataset.replace("_", " "), s=100, edgecolor="black", linewidth=1
-                )
-                ax.plot(x_pos, metric_values, linestyle="-", alpha=0.6)
+                ax.scatter(x_pos, metric_values, label=dataset.replace('_', ' '), s=100, edgecolor='black', linewidth=1)
+                ax.plot(x_pos, metric_values, linestyle='-', alpha=0.6)
 
         ax.set_xticks(range(len(model_names)))
-        ax.set_xticklabels(model_names, rotation=45, fontsize=15)
-        ax.tick_params(axis="y", labelsize=15)
+        ax.set_xticklabels(model_names, rotation=45, fontsize=19)
+        ax.tick_params(axis='y', labelsize=19)
         ax.set_ylabel(metric)
 
         ax.set_ylim(0, 1)
-        ax.legend(title="Datasets", loc="upper left", bbox_to_anchor=(1, 1))
+        ax.legend(title="Datasets", loc='upper left', bbox_to_anchor=(1, 1))
 
     plt.tight_layout()
-    plt.savefig("dotplot.svg")
-    plt.show()
+    plt.close(fig)
+    return fig
+
