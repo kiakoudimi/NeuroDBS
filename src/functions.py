26ecf7819b137d3168be43de98c9d46bcd711837
kiakoud <kiakoud@cbs.mpg.de>
2026-03-02 13:10:12 +0100

    update

--- src/functions.py
+++ src/functions.py
@@ -1,44 +1,47 @@
 """Functions used in the analyses."""
 
 import matplotlib.pyplot as plt
-
-# import nibabel as nib
-# import nilearn.masking
-# import numpy as np
+import nibabel as nib
+import nilearn.masking
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from aeon.visualisation import plot_critical_difference, plot_significance
 from scipy.stats import friedmanchisquare
 
-
 def plot_auc_heatmap(file_path, model_list):
+    
     xls = pd.ExcelFile(file_path)
 
     dataset_list = []
     auc_list = []
 
-    # Get performances per feature map
+    # Get perfrormences per feature map
     for sheet_name in xls.sheet_names:
         df = pd.read_excel(xls, sheet_name=sheet_name)
 
-        # Get model names
+        #Get model names
         models = df.iloc[:, 0]
 
-        # Get AUC scores
+        #Get AUC scores
         mean_auc = df.iloc[:, 1]
 
         # Add data to the lists
-        dataset_list.extend([sheet_name] * len(models))
+        dataset_list.extend([sheet_name] * len(models))  
         auc_list.extend(mean_auc)
 
     # Results export
-    auc_long = pd.DataFrame({"Model": model_list, "Dataset": dataset_list, "AUC": auc_list})
+    auc_long = pd.DataFrame({
+        'Model': model_list,
+        'Dataset': dataset_list,
+        'AUC': auc_list
+    })
 
     # Remove underscores from names
-    auc_long["Dataset"] = auc_long["Dataset"].str.replace("_", " ", regex=False)
+    auc_long['Dataset'] = auc_long['Dataset'].str.replace('_', ' ', regex=False)
 
-    # Plot figure heatmap
-    fig = plt.figure(figsize=(13, 8))
+    #Plot figure heatmap
+    fig =plt.figure(figsize=(13, 8))
     ax = sns.scatterplot(
         data=auc_long,
         x="Dataset",
@@ -48,32 +51,31 @@ def plot_auc_heatmap(file_path, model_list):
         palette="RdYlBu",
         marker="s",
         sizes=(1000, 2400),
-        legend=False,
+        legend=False  
     )
 
     # Add text annotations
     for i in range(len(auc_long)):
         ax.text(
-            x=auc_long["Dataset"][i],
-            y=auc_long["Model"][i],
-            s=f"{auc_long['AUC'][i]:.2f}",
-            color="black",
-            ha="center",
-            va="center",
-            fontsize=13,
+        x=auc_long["Dataset"][i],
+        y=auc_long["Model"][i],
+        s=f"{auc_long['AUC'][i]:.2f}",
+        color="black",
+        ha="center", va="center",
+        fontsize=13
         )
 
-    norm = plt.Normalize(vmin=auc_long["AUC"].min(), vmax=auc_long["AUC"].max())
+    norm = plt.Normalize(vmin=auc_long['AUC'].min(), vmax=auc_long['AUC'].max())
     sm = plt.cm.ScalarMappable(cmap="RdYlBu", norm=norm)
     sm.set_array([])
 
     # Add the colorbar with min/max ticks
     cbar = plt.colorbar(sm, ax=ax)
-    cbar.set_label("AUC Score", fontsize=15)
+    cbar.set_label('AUC Score', fontsize=15)
 
-    # Round the min and max AUC values
-    min_auc_rounded = round(auc_long["AUC"].min(), 2)
-    max_auc_rounded = round(auc_long["AUC"].max(), 2)
+    # Round the min and max AUC values 
+    min_auc_rounded = round(auc_long['AUC'].min(), 2)
+    max_auc_rounded = round(auc_long['AUC'].max(), 2)
 
     # Set ticks to show the rounded min and max values
     cbar.set_ticks([min_auc_rounded, max_auc_rounded])
@@ -82,13 +84,13 @@ def plot_auc_heatmap(file_path, model_list):
     plt.xticks(fontsize=15, rotation=45)
     plt.yticks(fontsize=15)
 
-    plt.tight_layout()
+    plt.tight_layout()  
     plt.close(fig)
     
     return fig
 
-
 def plot_metrics(file_path, metrics, model_names):
+
     # Read data
     xls = pd.ExcelFile(file_path)
     datasets = xls.sheet_names
@@ -98,7 +100,7 @@ def plot_metrics(file_path, metrics, model_names):
     # Get data
     for dataset in datasets:
         df = xls.parse(dataset)
-        df = df.set_index(df.columns[0])
+        df.set_index(df.columns[0], inplace=True)
         df.index = model_names
 
         # Extract values for each metric
@@ -108,27 +110,25 @@ def plot_metrics(file_path, metrics, model_names):
 
     # Create subplots for each metric
     fig, axes = plt.subplots(len(metrics), 1, figsize=(7, 5.3 * len(metrics)))
+    colors = list(plt.cm.tab10.colors) + ['#D3D3D3'] 
 
     # Loop through each metric and plot it
     for idx, metric in enumerate(metrics):
         ax = axes[idx]
-        # for i, dataset in enumerate(datasets):
-        for dataset in datasets:
+        for i, dataset in enumerate(datasets):
             metric_values = data_for_plot[metric].get(dataset)
             if metric_values is not None:
                 x_pos = range(len(model_names))
-                ax.scatter(
-                    x_pos, metric_values, label=dataset.replace("_", " "), s=100, edgecolor="black", linewidth=1
-                )
-                ax.plot(x_pos, metric_values, linestyle="-", alpha=0.6)
+                ax.scatter(x_pos, metric_values, label=dataset.replace('_', ' '), s=100, edgecolor='black', linewidth=1, color=colors[i])
+                ax.plot(x_pos, metric_values, linestyle='-', alpha=0.6, color=colors[i])
 
         ax.set_xticks(range(len(model_names)))
         ax.set_xticklabels(model_names, rotation=45, fontsize=19)
-        ax.tick_params(axis="y", labelsize=19)
+        ax.tick_params(axis='y', labelsize=19)
         ax.set_ylabel(metric)
 
         ax.set_ylim(0, 1)
-        ax.legend(title="Datasets", loc="upper left", bbox_to_anchor=(1, 1))
+        ax.legend(title="Datasets", loc='upper left', bbox_to_anchor=(1, 1))
 
     plt.tight_layout()
     plt.close(fig)
@@ -224,3 +224,95 @@ def get_feature_maps(feature_importances_path, measures, mask, output):
             nib.save(importance_nii, output + f'{measure}_{model_name}_importance_map.nii.gz')
             
     return
+
+def plot_violinplots(y_proba, y_true, clinical_scores):
+    
+    y_pred = (y_proba >= 0.5).astype(int)
+
+    y_true_paired = np.column_stack((y_true[:34], y_true[34:]))   
+    y_pred_paired = np.column_stack((y_pred[:34], y_pred[34:]))  
+    subject_perf = (y_true_paired == y_pred_paired).mean(axis=1)
+    
+    df = pd.DataFrame({
+    'Accuracy': subject_perf,       
+    'Improvement': clinical_scores
+    })
+
+    df = df.dropna()
+
+    df['Accuracy'] = pd.Categorical(df['Accuracy'], categories=[0.0, 0.5, 1.0], ordered=True)
+
+    fig = plt.figure(figsize=(6,5))
+    
+    sns.violinplot(
+        x='Accuracy',
+        y='Improvement',
+        data=df,
+        cut=0,             
+        color='white',    
+        linewidth=1.5
+    )
+
+    sns.stripplot(
+        x='Accuracy',
+        y='Improvement',
+        data=df,
+        color='black',
+        size=8,
+        jitter=True,
+        alpha=0.7
+    )
+
+    plt.xlabel('Accuracy')
+    plt.ylabel('MDS-UPDRS III Improvement (%)')
+    plt.close(fig)
+    
+    return fig
+
+def plot_scatterplots(y_proba, y_true, clinical_scores_on, clinical_scores_off, clinical_scores_improvement):
+ 
+    y_pred = (y_proba >= 0.5).astype(int)
+
+    y_true_paired = np.column_stack((y_true[:34], y_true[34:]))   
+    y_pred_paired = np.column_stack((y_pred[:34], y_pred[34:]))  
+    subject_perf = (y_true_paired == y_pred_paired).mean(axis=1)
+    
+    mask = ~np.isnan(clinical_scores_off) & ~np.isnan(clinical_scores_on) & ~np.isnan(clinical_scores_improvement) & ~np.isnan(subject_perf)
+    mds_off_clean = mds_off[mask]
+    mds_on_clean = mds_on[mask]
+    mds_improvement_clean = clinical_scores_improvement[mask]
+    subject_perf_clean = subject_perf[mask]
+ 
+    marker_map = {0: '*', 0.5: 'o', 1: '^'}
+
+    fig, ax = plt.subplots(figsize=(7, 5))
+    vmin = np.min(mds_improvement_clean)
+    vmax = np.max(mds_improvement_clean)
+
+    for acc in np.unique(subject_perf_clean):
+        idx = subject_perf_clean == acc
+        sc = ax.scatter(
+            mds_off_clean[idx],
+            mds_on_clean[idx],
+            c=mds_improvement_clean[idx],
+            cmap='YlOrBr_r',
+            marker=marker_map[acc],
+            s=120,
+            edgecolor='k',
+            label=f'Accuracy {acc}',
+            vmin=vmin,
+            vmax=vmax
+        )
+
+    ax.set_xlabel('MDS-UPDRS III OFF')
+    ax.set_ylabel('MDS-UPDRS III ON')
+
+    sm = plt.cm.ScalarMappable(cmap='YlOrBr_r', norm=plt.Normalize(vmin=vmin, vmax=vmax))
+    sm.set_array([])
+    cbar = fig.colorbar(sm, ax=ax)
+    cbar.set_label('Clinical improvement (%)')
+
+    ax.legend(title='')
+    plt.close(fig)
+    
+    return fig
